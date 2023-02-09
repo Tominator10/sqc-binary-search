6d0f8918f4d24a5cd88816f96969dc38e3109cf7
Tominator10 <Tominator10@users.noreply.github.com>
2023-02-08 19:26:13 -0600

    Add function tests

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const binarySearch = function (sortedArray, target) {
 
-}
+
+};
+
+console.log("Tests:");
+console.log();
+console.log("searching [1,2,3,4,5] for 5");
+binarySearch([1, 2, 3, 4, 5], 5);
+console.log();
+console.log("searching [1, 2, 2, 8] for 8");
+binarySearch([1, 2, 2, 8], 8);
+console.log();
+console.log("searching [1, 2, 13, 26, 37] for 46");
+binarySearch([1, 2, 13, 26, 37], 46);
